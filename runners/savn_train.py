f38ba5493a7f2c08ea0b055f54a4442d2fcda75d
wanjiaqi <135974821+wanjiaqi7@users.noreply.github.com>
2024-07-10 19:07:11 +0800

    Update savn_train.py

--- runners/savn_train.py
+++ runners/savn_train.py
@@ -1,3 +1,4 @@
+# 实现了一个多进程强化学习训练的主循环，其中每个进程都独立地进行训练，并在每个训练步骤后更新共享的模型参数
 from __future__ import division
 
 import time
@@ -22,26 +23,26 @@ from .train_util import (
 
 
 def savn_train(
-    rank,
-    args,
-    create_shared_model,
-    shared_model,
-    initialize_agent,
-    optimizer,
-    res_queue,
-    end_flag,
+    rank,                      # 当前进程的标识符，用于在多进程训练中区分不同的进程
+    args,                      # 训练参数，包含训练所需的各种配置信息
+    create_shared_model,       # 创建共享模型的函数
+    shared_model,              # 共享模型，在多进程训练中所有进程共享该模型
+    initialize_agent,          # 初始化智能体的函数
+    optimizer,                 # 优化器，用于更新模型参数
+    res_queue,                 # 结果队列，用于进程间通信
+    end_flag,                  # 结束标志，指示训练何时结束
 ):
-
-    glove = Glove(args.glove_file)
-    scenes, possible_targets, targets = get_data(args.scene_types, args.train_scenes)
-
-    random.seed(args.seed + rank)
+    # 初始化
+    glove = Glove(args.glove_file)             # 加载 GloVe 词向量文件，用于处理文本数据
+    scenes, possible_targets, targets = get_data(args.scene_types, args.train_scenes)           #  获取训练场景、可能的目标和实际目标
+ 
+    random.seed(args.seed + rank)                           # 设置随机种子以确保结果可重复
     idx = [j for j in range(len(args.scene_types))]
     random.shuffle(idx)
 
-    setproctitle.setproctitle("Training Agent: {}".format(rank))
+    setproctitle.setproctitle("Training Agent: {}".format(rank))          # 设置进程标题，以便在系统监控中区分不同的训练进程
 
-    gpu_id = args.gpu_ids[rank % len(args.gpu_ids)]
+    gpu_id = args.gpu_ids[rank % len(args.gpu_ids)]          # 为当前进程分配的 GPU ID
 
     import torch
 
@@ -50,16 +51,17 @@ def savn_train(
     if gpu_id >= 0:
         torch.cuda.manual_seed(args.seed + rank)
 
-    player = initialize_agent(create_shared_model, args, rank, gpu_id=gpu_id)
+    player = initialize_agent(create_shared_model, args, rank, gpu_id=gpu_id)         # 初始化智能体
 
-    model_options = ModelOptions()
-
+    model_options = ModelOptions()       # 模型选项，存储模型参数
+   
+    # 训练循环
     j = 0
 
     while not end_flag.value:
 
         start_time = time.time()
-        new_episode(
+        new_episode(                                # 通过 new_episode 函数初始化一个新的训练场景
             args, player, scenes[idx[j]], possible_targets, targets[idx[j]], glove=glove
         )
         player.episode.exploring = True
@@ -122,7 +124,7 @@ def savn_train(
             print("meta gradient")
 
         # Compute the meta_gradient, i.e. differentiate w.r.t. theta.
-        meta_gradient = torch.autograd.grad(
+        meta_gradient = torch.autograd.grad(                 
             loss["total_loss"],
             [v for _, v in params_list[0].items()],
             allow_unused=True,
